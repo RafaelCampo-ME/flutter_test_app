99e9e4a920692d54c25cf83d4903fede5af33f38
Rafael <rs.campo10@uniandes.edu.co>
2022-12-29 22:50:06 -0500

    BackLayout

---
+++ flutter_application_1/lib/BackWheater.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '/assets/ui_constants.dart';
+
+class BackWeather extends StatelessWidget {
+  @override 
+  Widget  build(BuildContext context){
+      //TODO: Implement build
+      return new Stack(
+        children: <Widget> [ 
+          GradientBack(),
+          new Positioned(
+            
+            bottom:0.0,
+
+            child: Container (
+            width: MediaQuery.of(context).size.width,
+            height: 125.0,
+            color: Colors.white,
+            
+            
+            ))
+          
+          
+          ],
+      );
+    }
+}
+
+class GradientBack extends StatelessWidget {
+  @override build(BuildContext context){
+    return new Container(
+      decoration: new BoxDecoration(
+        gradient:  new LinearGradient(colors: [BackBlue, BackGreen],
+                                      begin: const FractionalOffset(1.0, 0.1),
+                                      end: const FractionalOffset(1.0, 0.9)
+          )
+      ),
+    );
+  }
+}
---
+++ flutter_application_1/lib/assets/ui_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const BackBlue = Color.fromRGBO( 83, 151, 166,100);   // #5397A6
+const BackGreen = Color.fromRGBO( 54, 89, 73,100);    //  #365949
+const BackPurple = Color.fromRGBO(74, 84, 140,100);  //  #4A548C
+const BackBone = Color.fromRGBO(242, 233, 216,100);  //  #F2E9D8
+const BackBlack = Color.fromRGBO(13,13,13,100);      //  #0D0D0D
