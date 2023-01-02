464da2d1a105e2c35e75cea8d99f7ff48ff680f2
Rafael <rs.campo10@uniandes.edu.co>
2023-01-02 11:12:42 -0500

    Changes in profile

--- flutter_application_1/lib/profile.dart
+++ flutter_application_1/lib/profile.dart
@@ -1,6 +1,6 @@
-import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/BackWheater.dart';
+import 'package:flutter_application_1/assets/ui_constants.dart';
 
 class Profile extends StatelessWidget {
 
@@ -13,7 +13,7 @@ static const routeName = "/profile";
       body: Column(
         children: [
           BackWeather(),
-          ProfileHeader(),
+          SizedBox(height: 40.0, child: ProfileHeader()),
           ElevatedButton(
                         onPressed: null /*Aun no hace nada*/ , 
                         child: const Text('vamos a la pantalla de inicio')
